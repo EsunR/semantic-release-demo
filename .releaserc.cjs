c4f13d31449cc40ba1d3adb57931b377b7a71e44
EsunR <esunr641@icloud.com>
2023-09-12 23:50:44 +0800

    build: fix semantic-release problem

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -1,10 +1,17 @@
 module.exports = {
+  ci: false,
   branches: ["master"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/npm",
-    "@semantic-release/git",
+    [
+      "@semantic-release/git",
+      {
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]",
+      },
+    ],
   ],
 };
