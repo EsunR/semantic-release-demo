d9bcbd21659e4a6963dacd0790877f9dfaca5759
EsunR <esunr641@icloud.com>
2023-09-12 23:53:45 +0800

    feat: add reduce function

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ console.log("Hello, world!");
 function acc(a, b) {
   return a + b;
 }
+
+function reduce(arr, fn, initial) {
+  let acc = initial;
+  for (let i = 0; i < arr.length; i++) {
+    acc = fn(acc, arr[i]);
+  }
+  return acc;
+}
